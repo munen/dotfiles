6ef8d1a55f76207e353ae966aa81db1784e9cd37
Alain M. Lafon <alain@200ok.ch>
2022-11-11 10:31:42 +0100

    feat: Move more things into guix
          - Configure nonguix channel
          - Split into different manifest

--- guix/.config/guix/manifests/manifest.scm
+++ guix/.config/guix/manifests/manifest.scm
@@ -1,23 +1,13 @@
-;; What follows is a "manifest" equivalent to the command line you gave.
-;; You can store it in a file that you may then pass to any 'guix' command
-;; that accepts a '--manifest' (or '-m') option.
-
 (specifications->manifest (list "git"
+                                "git-lfs"
                                 "openssh"
 
                                 "gnupg"
                                 "pinentry"
 
                                 "icecat"
-                                "pavucontrol"
-                                "xbindkeys"
-                                "polybar"
 
                                 "keepassxc"
-
-                                "polybar"
-                                "kitty"
-                                "rofi"
 
                                 "stow"
 
@@ -34,4 +24,4 @@
                                 "emacs-spaceline"
                                 "hunspell" "hunspell-dict-de"
 
-                                "pulseaudio"))
+                                ))
