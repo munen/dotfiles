7d52a92e95785e4a4291fe729e4cd6f68a9c7fb4
Alain M. Lafon <alain@200ok.ch>
2022-11-11 10:31:42 +0100

    feat: Move more things into guix
          - Configure nonguix channel
          - Split into different manifest

---
+++ guix/.config/guix/manifests/i3.scm
@@ -0,0 +1,14 @@
+(specifications->manifest (list "i3-wm"              ; @4.20.1
+                                "polybar"
+                                "hicolor-icon-theme" ; @0.17
+                                "adwaita-icon-theme" ; @42.0
+
+                                "dunst"              ; @1.9.0
+                                "xbindkeys"
+                                "libnotify"          ; notify-send, @0.7.9
+
+                                "turbovnc"
+
+                                "kitty"
+                                "rofi"
+                                ))
