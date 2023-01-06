bfe1f8721b7c3f81f9f082bd440a87efb6bcac37
Alain M. Lafon <alain@200ok.ch>
2023-01-06 20:10:37 +0100

    chore: Improve album downloader

--- bin/bin/sync_albums.rb
+++ bin/bin/sync_albums.rb
@@ -2,36 +2,49 @@
 # coding: utf-8
 # frozen_string_literal: true
 
+# https://github.com/icloud-photos-downloader/icloud_photos_downloader
+
 require 'gpgme'
 
 passwords = GPGME::Crypto.decrypt(File.read("#{Dir.home}/.icloud_credentials.gpg"))
 
 passwords.to_s.each_line do |line|
   if line =~ /^([^ ]+) ([^ ]+)/
-    @login = "#{$1}"
-    @password = "#{$2}"
+    @login = "#{$1.strip}"
+    @password = "#{$2.strip}"
   end
 end
 
-albums = `docker run -it --rm --name icloud \
-    -v /home/munen/Dropbox/_PHOTOS/ALBUMS/:/data \
+cmd = "docker run -it --rm --name icloud \
     -v /home/munen/.icloudpd/cookies:/cookies \
     -e TZ=Europe/Zurich \
     icloudpd/icloudpd:latest \
-    icloudpd --directory /data \
+    icloudpd \
     --log-level info \
     --cookie-directory /cookies \
     --username #{@login} \
-    --password  #{@password} \
-    --size original \
-    --list-albums`
+    --password #{@password} \
+    --list-albums"
 
+# Open a pipe to the `docker` command
+docker_pipe = IO.popen cmd
+
+albums = []
+
+# Read from the pipe in real time, writing each line to stdout
+docker_pipe.each do |line|
+  puts line
+  albums << line.strip
+end
+
+# Close the pipe
+docker_pipe.close
 
 status = $?.exitstatus
 
 exit "Quitting due to exit status: '#{status}' and response '#{albums}'" unless status == 0
 
-for album in albums.split
+for album in albums
   if album.start_with? "#{Time.new.year}"
     puts ""
     puts "Downloading '#{album}'"
@@ -40,6 +53,7 @@ for album in albums.split
      -v /home/munen/Dropbox/_PHOTOS/ALBUMS/:/data \
      -v /home/munen/.icloudpd/cookies:/cookies \
      -e TZ=Europe/Zurich \
+     -e USER_ID=$(echo "$UID") \
      icloudpd/icloudpd:latest \
      icloudpd --directory /data \
      --cookie-directory /cookies \
