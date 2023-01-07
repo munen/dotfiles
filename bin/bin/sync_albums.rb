318248090d70742690e3fe8a23333efdc8405812
Alain M. Lafon <alain@200ok.ch>
2023-01-07 15:37:27 +0100

    feat: Finalize sync_albums.rb

--- bin/bin/sync_albums.rb
+++ bin/bin/sync_albums.rb
@@ -4,6 +4,7 @@
 
 # https://github.com/icloud-photos-downloader/icloud_photos_downloader
 
+# BEGIN: Get icloud credentials
 require 'gpgme'
 
 passwords = GPGME::Crypto.decrypt(File.read("#{Dir.home}/.icloud_credentials.gpg"))
@@ -14,6 +15,17 @@ passwords.to_s.each_line do |line|
     @password = "#{$2.strip}"
   end
 end
+# END: Get icloud credentials
+
+# BEGIN: Helper methods
+def parse_album_name(str)
+  year, month, day, description = str.match(/^(\d{4})?-?(\d{2})?-?(\d{2})?_?(.*)?/).captures
+
+  { year: year, month: month, day: day, description: description, title: str }
+end
+
+# END: Helper methods
+
 
 cmd = "docker run -it --rm --name icloud \
     -v /home/munen/.icloudpd/cookies:/cookies \
@@ -33,8 +45,7 @@ albums = []
 
 # Read from the pipe in real time, writing each line to stdout
 docker_pipe.each do |line|
-  puts line
-  albums << line.strip
+  albums << parse_album_name(line.strip)
 end
 
 # Close the pipe
@@ -44,25 +55,43 @@ status = $?.exitstatus
 
 exit "Quitting due to exit status: '#{status}' and response '#{albums}'" unless status == 0
 
-for album in albums
-  if album.start_with? "#{Time.new.year}"
-    puts ""
-    puts "Downloading '#{album}'"
+if ARGV[0] == '-l'
+  puts "Listing albums:\n\n-----\n"
+  albums.each { |album| puts album[:title] }
+  exit 0
+end
 
-    res = `docker run -it --rm --name icloud \
-     -v /home/munen/Dropbox/_PHOTOS/ALBUMS/:/data \
+# Reduce down to relevant albums that should be downloaded
+albums.select! { |album| album[:year] and album[:month] }
+
+puts "\nDownloading album:"
+for album in albums
+  folder_structure = if album[:day]
+                      "#{album[:year]}/#{album[:month]}/#{album[:day]}/#{album[:description]}"
+                    else
+                      "#{album[:year]}/#{album[:month]}/#{album[:description]}"
+                    end
+
+  puts "'#{album[:title]}' into folder: '#{folder_structure}'"
+
+  cmd = "docker run -it --rm --name icloud \
+     --user $(id -u):$(id -g) \
+     -v /home/munen/Dropbox/_PHOTOS/by_date/:/data \
      -v /home/munen/.icloudpd/cookies:/cookies \
      -e TZ=Europe/Zurich \
-     -e USER_ID=$(echo "$UID") \
      icloudpd/icloudpd:latest \
      icloudpd --directory /data \
      --cookie-directory /cookies \
-     --folder-structure {:%Y/%m}/#{album} \
+     --folder-structure #{folder_structure} \
      --username #{@login} \
      --password  #{@password} \
      --size original \
-     --album #{album}`
+     --album #{album[:title]}"
 
-    puts res
-  end
+  res = `#{cmd}`
+  status = $?.exitstatus
+
+  exit "Quitting due to exit status: '#{status}' and response '#{res}'" unless status == 0
+
+  puts res
 end
