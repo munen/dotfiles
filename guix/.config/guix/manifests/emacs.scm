e95e5587f8006c6deab1728ed3a6b4d248149c44
Alain M. Lafon <alain@200ok.ch>
2023-02-21 09:52:10 +0100

    chore: Add calendar and contacts links

--- guix/.config/guix/manifests/emacs.scm
+++ guix/.config/guix/manifests/emacs.scm
@@ -1,8 +1,6 @@
 (specifications->manifest
  '("emacs"
    "emacs-org-auto-tangle"
-   "emacs-magit"
-   ;; "emacs-forge" ; temporarily disable, because it cannot be built
    "mu"
    "emacs-spaceline"
    "font-fira-code"
