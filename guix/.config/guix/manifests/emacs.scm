db2754ebdd1f9e3cf2aaea1476fce4d5875652ce
Alain M. Lafon <alain@200ok.ch>
2023-11-18 17:11:29 +0100

    chore: More dependencies via GUIX

--- guix/.config/guix/manifests/emacs.scm
+++ guix/.config/guix/manifests/emacs.scm
@@ -1,17 +1,15 @@
-(specifications->manifest
+﻿(specifications->manifest
  '("emacs"
    "emacs-org-auto-tangle"
+   "emacs-undo-tree"
+   "xclip"
+   "xdotool"
+   "xprop"
+   "xwininfo"
    "mu"
    "emacs-spaceline"
    "font-fira-code"
    "font-google-noto"
    "emacs-spacemacs-theme"
    "emacs-hl-todo"
-
-   ;; Install emacs-everywhere dependencies
-   ;; https://github.com/tecosaur/emacs-everywhere/
-   "xclip"
-   "xdotool"
-   "xprop"
-   "xwininfo"
-   ))
+))
