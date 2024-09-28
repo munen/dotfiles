5b76faef37cb814d4a1c54f809d73aaf1b3eb121
Alain M. Lafon <alain@200ok.ch>
2024-09-28 12:04:35 +0200

    chore: Binstubs for icloud + minor fix for recgif.sh

--- bin/bin/recgif.sh
+++ bin/bin/recgif.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Error: No output filename provided"
+    echo "Usage: $0 output_filename.gif"
+    exit 1
+fi
+
 geometry=$(slop -f "-video_size %wx%h -i :0.0+%x,%y")
 ffmpeg -framerate 30 -f x11grab $geometry "$1"
