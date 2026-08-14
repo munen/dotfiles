71e815a91b58a396614233779cc5510733124d5a
Munen Alain M. Lafon <alain@200ok.ch>
2026-08-14 16:47:52 +0200

    fix(polybar): avoid tray race on restart

--- bin/bin/launch_polybar.sh
+++ bin/bin/launch_polybar.sh
@@ -2,13 +2,16 @@
 
 # Terminate already running bar instances
 killall -q polybar
+while pgrep -u "$UID" -x polybar >/dev/null; do
+  sleep 0.1
+done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example 2>&1 | tee -a /tmp/polybar.log & disown
+    MONITOR=$m polybar --reload example >>/tmp/polybar.log 2>&1 &
   done
 else
-  polybar --reload example 2>&1 | tee -a /tmp/polybar.log & disown
+  polybar --reload example >>/tmp/polybar.log 2>&1 &
 fi
 
 echo "Polybar launched..."
